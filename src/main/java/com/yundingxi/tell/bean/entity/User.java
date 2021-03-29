3e4e7e2b7c192b378f9a40ed7600b1012ce224d4
Alsa <alsa@xx.com.cn>
2021-03-29 18:10:37 +0800

    解决冲突

--- src/main/java/com/yundingxi/tell/bean/entity/User.java
+++ src/main/java/com/yundingxi/tell/bean/entity/User.java
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 package com.yundingxi.tell.bean.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 import java.io.Serializable;
-import java.util.UUID;
-
 /**
  * (User)实体类
  *
@@ -56,55 +50,3 @@ public class User implements Serializable {
     private String avatarUrl;
 
 }
-=======
-package com.yundingxi.tell.bean.entity;
-
-import java.util.Date;
-import java.io.Serializable;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-/**
- * @author houdongsheng
- * @since 2021-03-28
- */
-@Data
-@EqualsAndHashCode(callSuper = false)
-@ApiModel(value="User对象", description="")
-@AllArgsConstructor
-@NoArgsConstructor
-public class User implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @ApiModelProperty(value = "用户组件")
-    private String openId;
-
-    @ApiModelProperty(value = "出生日期")
-    private Date dateBirth;
-
-    @ApiModelProperty(value = "0：女 1：男")
-    private Integer gender;
-
-    @ApiModelProperty(value = "注册时间")
-    private Date registrationTime;
-
-    @ApiModelProperty(value = "最后一次登陆时间")
-    private Date lastLoginTime;
-
-    @ApiModelProperty(value = "状态 0  正常，1 停用")
-    private Integer state;
-
-    @ApiModelProperty(value = "笔名")
-    private String penName;
-
-    @ApiModelProperty(value = "头像url")
-    private String avatarUrl;
-
-
-}
->>>>>>> bfdd24e1169c7ec6f002af6ffa3a5e9485617afe
--- src/main/java/com/yundingxi/tell/controller/UserController.java
+++ src/main/java/com/yundingxi/tell/controller/UserController.java
@@ -7,7 +7,7 @@ import com.yundingxi.tell.util.ResultGenerator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
 /**
