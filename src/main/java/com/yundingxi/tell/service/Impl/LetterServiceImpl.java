8e5d2b5f8392eb2946cc0e29fb72833c02d8276d
comcloud <2230817302@qq.com>
2021-05-05 23:43:06 +0800

    存储回复信件时候更改为存储时间为年月日时分秒

--- src/main/java/com/yundingxi/tell/service/Impl/LetterServiceImpl.java
+++ src/main/java/com/yundingxi/tell/service/Impl/LetterServiceImpl.java
@@ -143,7 +143,7 @@ public class LetterServiceImpl implements LetterService {
         CompletableFuture.runAsync(() -> {
             String replyId = UUID.randomUUID().toString();
             Reply reply = new Reply(replyId, letterReplyDto.getLetterId(), new Date(), letterReplyDto.getMessage(), letterReplyDto.getSender(), letterReplyDto.getSenderPenName());
-            String replyTime = LocalDate.now(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String replyTime = LocalDate.now(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
             letterMapper.insertReply(reply);
             @SuppressWarnings("unchecked") List<UnreadMessageDto> messageDtoList = (List<UnreadMessageDto>) redisUtil.get("letter:" + letterReplyDto.getRecipient() + ":unread_message");
             UnreadMessageDto messageDto = new UnreadMessageDto(letterReplyDto.getSender()
