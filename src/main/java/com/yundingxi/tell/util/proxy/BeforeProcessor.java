92d430d2c12317dedde4ccd682fa36f121413cca
comcloud <2230817302@qq.com>
2021-06-26 17:05:44 +0800

    代理工具类

---
+++ src/main/java/com/yundingxi/tell/util/proxy/BeforeProcessor.java
@@ -0,0 +1,13 @@
+package com.yundingxi.tell.util.proxy;
+
+/**
+ * 前置增强
+ * @version v1.0
+ * @ClassName PreEnhancement
+ * @Author rayss
+ * @Datetime 2021/6/20 5:08 下午
+ */
+
+public interface BeforeProcessor {
+    void beforeProcess();
+}
---
+++ src/main/java/com/yundingxi/tell/util/proxy/CustomInvocationHandler.java
@@ -0,0 +1,58 @@
+package com.yundingxi.tell.util.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @version v1.0
+ * @ClassName CustomInvocationHandler
+ * @Author rayss
+ * @Datetime 2021/6/26 4:47 下午
+ */
+
+public class CustomInvocationHandler implements InvocationHandler {
+
+    /**
+     * 增强对象
+     */
+    private final Object proxyObject;
+
+    /**
+     * 前置增强
+     */
+    private BeforeProcessor beforeProcessor;
+    /**
+     * 后置增强
+     */
+    private AfterProcessor afterProcessor;
+
+    public CustomInvocationHandler(Object proxyObject) {
+        this.proxyObject = proxyObject;
+    }
+
+    /**
+     * 获取代理对象方法
+     * @param beforeProcessor 前置增强
+     * @param afterProcessor 后置增强
+     * @return 增强后的代理对象
+     */
+    public Object getProxyObject(BeforeProcessor beforeProcessor, AfterProcessor afterProcessor) {
+        this.beforeProcessor = beforeProcessor;
+        this.afterProcessor = afterProcessor;
+        return Proxy.newProxyInstance(CustomInvocationHandler.class.getClassLoader(), proxyObject.getClass().getInterfaces(), this);
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (beforeProcessor != null) {
+            beforeProcessor.beforeProcess();
+        }
+        Object methodObject = method.invoke(proxyObject, args);
+        if (afterProcessor != null) {
+            afterProcessor.afterProcess();
+        }
+        return methodObject;
+    }
+}
